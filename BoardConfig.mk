8723a8a1541d10a124e42f2026fad1dafdbb2e63
lopestorm <guillopes2002@gmail.com>
2022-12-21 16:55:04 -0300

    Add files via upload

---
+++ BoardConfig.mk
@@ -0,0 +1,224 @@
+#
+# Copyright (C) 2022 The TWRP Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/infinix/X676C
+
+# Building with minimal manifest
+ALLOW_MISSING_DEPENDENCIES := true
+
+# Build Hack
+BUILD_BROKEN_DUP_RULES := true
+
+# A/B
+AB_OTA_UPDATER := true
+
+AB_OTA_PARTITIONS += \
+	boot \
+	dtbo \
+	system \
+	product \
+	vendor \
+	vbmeta \
+	vendor_boot \
+	vbmeta_system \
+	vbmeta_vendor
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a55
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a55
+
+ENABLE_CPUSETS := true
+ENABLE_SCHEDBOOST := true
+
+# Assertation
+TARGET_OTA_ASSERT_DEVICE := X676C-OP,Infinix X676C
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := x676c_h891
+#TARGET_BOOTLOADER_BOARD_NAME := CY-X697-H812-A
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6789
+PRODUCT_PLATFORM := mt6789
+
+# Kernel
+BOARD_VENDOR_CMDLINE := bootopt=64S3,32N2,64N2
+BOARD_PAGE_SIZE := 4096
+BOARD_KERNEL_BASE := 0x3fff8000
+BOARD_KERNEL_OFFSET := 0x00008000
+BOARD_RAMDISK_OFFSET := 0x26f08000
+BOARD_TAGS_OFFSET := 0x07c88000
+BOARD_BOOT_HEADER_VERSION := 4
+BOARD_DTB_SIZE := 191249
+BOARD_DTB_OFFSET := 0x07c88000
+BOARD_HEADER_SIZE := 2128
+
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+
+TARGET_NO_KERNEL := true
+BOARD_RAMDISK_USE_LZ4 := true
+BOARD_KERNEL_SEPARATED_DTBO := true
+
+TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb
+
+BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
+BOARD_MKBOOTIMG_ARGS += --vendor_cmdline $(BOARD_VENDOR_CMDLINE)
+BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_PAGE_SIZE) --board ""
+BOARD_MKBOOTIMG_ARGS += --kernel_offset $(BOARD_KERNEL_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_TAGS_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
+BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
+
+# Partitions
+BOARD_FLASH_BLOCK_SIZE := 262144
+# Compression Tuning https://source.android.com/docs/core/architecture/kernel/erofs
+# By default, EROFS compresses into fixed-size blocks. Compression efficacy can be increased significantly
+# by enabling variable-length blocks. This can be configured by the following flag:
+# BOARD_EROFS_PCLUSTER_SIZE := 262144
+# This sets the maximum "pcluster", or variable length block size, to 262144 bytes.
+# The number must be a multiple of 4096. There are diminishing returns at higher values, and higher
+# values can decrease read performance depending on the device hardware.
+
+BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := 67108864 # 0x4000000
+
+BOARD_SUPER_PARTITION_SIZE := 9126805504
+BOARD_SUPER_PARTITION_GROUPS := infinix_dynamic_partitions
+BOARD_INFINIX_DYNAMIC_PARTITIONS_PARTITION_LIST := system product vendor
+BOARD_INFINIX_DYNAMIC_PARTITIONS_SIZE := 9122611200 # (BOARD_SUPER_PARTITION_SIZE - 4194304) 4MiB
+
+BOARD_PARTITION_LIST := $(call to-upper, $(BOARD_INFINIX_DYNAMIC_PARTITIONS_PARTITION_LIST))
+$(foreach p, $(BOARD_PARTITION_LIST), $(eval BOARD_$(p)IMAGE_FILE_SYSTEM_TYPE := erofs))
+$(foreach p, $(BOARD_PARTITION_LIST), $(eval TARGET_COPY_OUT_$(p) := $(call to-lower, $(p))))
+
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
+
+# To enable EROFS, use the filesystem type "erofs" https://source.android.com/docs/core/architecture/kernel/erofs
+#BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := erofs
+#BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := erofs
+#BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := erofs
+#BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := erofs
+#BOARD_VENDOR_DLKMIMAGE_FILE_SYSTEM_TYPE := erofs
+#BOARD_SYSTEM_DLKMIMAGE_FILE_SYSTEM_TYPE := erofs
+
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+BOARD_ROOT_EXTRA_FOLDERS += tranfs
+
+# Treble
+BOARD_VNDK_VERSION := current
+TARGET_COPY_OUT_VENDOR := vendor
+TARGET_COPY_OUT_SYSTEM_EXT = system_ext
+TARGET_COPY_OUT_PRODUCT := product
+
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+BOARD_AVB_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
+BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
+
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+
+# Recovery
+TARGET_NO_RECOVERY := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
+
+# vendor_boot recovery ramdisk
+BOARD_USES_RECOVERY_AS_BOOT := 
+BOARD_USES_GENERIC_KERNEL_IMAGE := true
+BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
+BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE := 
+BOARD_MOVE_GSI_AVB_KEYS_TO_VENDOR_BOOT := 
+
+# Debug
+TARGET_USES_LOGD := true
+TWRP_INCLUDE_LOGCAT := true
+
+# Crypto
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO_FBE := true
+TW_INCLUDE_FBE_METADATA_DECRYPT := true
+BOARD_USES_METADATA_PARTITION := true
+
+# Encryption
+PLATFORM_SECURITY_PATCH := 2127-12-31
+BOOT_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
+VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
+PLATFORM_VERSION := 127
+PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
+
+# Tools
+TW_INCLUDE_RESETPROP := true
+TW_INCLUDE_LIBRESETPROP :=true
+TW_INCLUDE_REPACKTOOLS := true
+
+# USB
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.usb0/lun.%d/file
+
+# Display
+#TW_FRAMERATE := 60
+TW_THEME := portrait_hdpi
+TW_DEFAULT_BRIGHTNESS := 1000
+
+# Resolution
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
+# Configure Status bar icons for regular TWRP builds only - 1080 x 2400
+TW_CUSTOM_CLOCK_POS := 40
+TW_CUSTOM_CPU_POS := 605
+TW_STATUS_ICONS_ALIGN := center
+
+# Temp
+#TW_CUSTOM_CPU_TEMP_PATH := /sys/class/thermal/thermal_zone54/temp
+
+# Fastbootd
+TW_INCLUDE_FASTBOOTD := true
+
+# Language
+TW_EXTRA_LANGUAGES := false
+
+# Other TWRP Configurations
+TW_EXCLUDE_APEX := true
+TW_INCLUDE_NTFS_3G := true
+TARGET_USES_MKE2FS := true
+TW_INCLUDE_FUSE_EXFAT := true
+TW_INPUT_BLACKLIST := hbtp_vm
+TW_DEVICE_VERSION := Infinix Note 12 2023 X676C by lopestom
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+TW_DEFAULT_LANGUAGE := ru
